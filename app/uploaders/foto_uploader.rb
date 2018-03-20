aec4ce7f29eef8fa63c1bfbac91e7536ca1f5d07
aleks2414 <alejandroromo14@gmail.com>
2018-03-20 17:08:10 -0600

    images crop

--- app/uploaders/foto_uploader.rb
+++ app/uploaders/foto_uploader.rb
@@ -1,7 +1,7 @@
 class FotoUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -29,9 +29,13 @@ class FotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+     process resize_to_fit: [350, 350]
+  end
+
+  version :heavy do
+     process resize_to_fill: [1450, 400]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
